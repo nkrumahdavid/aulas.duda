44cba912dba5bfeace038fe4e9b9a33a52a2e801
David Nkrumah <46801561+nkrumahdavid@users.noreply.github.com>
2019-02-12 12:57:26 -0200

    sexta aula da Duda
    treinamento python

---
+++ aula06a.py
@@ -0,0 +1,31 @@
+print('Exemplo 01')
+num01 = input('Digite um valor: ')
+print(type(num01))
+
+print('Exemplo 02')
+num01 = int(input('Digite um valor: '))
+print(type(num01))
+
+print('Exemplo 03')
+num01 = int(input('Digite um valor: '))
+num02 = int(input('Digite outro valor: '))
+soma = num01 + num02
+print('A soma vale: ', soma)
+
+print('Exemplo 04')
+num01 = input('Digite um valor: ')
+num02 = input('Digite outro valor: ')
+soma = num01 + num02
+print('A soma vale: ', soma, 'Acertei?', 'Não?')
+
+print('Exemplo 05')
+num01 = int(input('Digite um valor: '))
+num02 = int(input('Digite outro valor: '))
+soma = num01 + num02
+print('A soma entre:', num01, 'e', num02, 'vale:', soma)
+
+print('Exemplo 06')
+num01 = int(input('Digite um valor: '))
+num02 = int(input('Digite outro valor: '))
+soma = num01 + num02
+print('A soma entre: {0} e {1} vale {2}'.format(num01, num02, soma))
